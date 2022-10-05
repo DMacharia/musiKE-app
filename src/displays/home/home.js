199396b81cf2e3ad7ce65639a6e1b851a033776e
Daniel Macharia <danielhuios@gmail.com>
2022-10-05 11:57:47 +0300

    created sidebar and styled

--- src/displays/home/home.js
+++ src/displays/home/home.js
@@ -1,15 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "../../components/sidebar/sidebar";
 import Favourites from "../favourites/favourites";
 import Feed from "../feed/feed";
 import Library from "../library/library";
 import Player from "../player/player";
 import Trending from "../trending/trending";
+import "./home.css"
 
 export default function Home() {
 	return (
 		<Router>
 			<div className="main-body">
+                <Sidebar/>
 				<Routes>
 					<Route path="/" element={<Library />} />
 					<Route path="/feed" element={<Feed />} />
@@ -17,7 +20,7 @@ export default function Home() {
 					<Route path="/player" element={<Player />} />
 					<Route path="/favourites" element={<Favourites />} />
 				</Routes>
-			</div>
+			j</div>
 		</Router>
 	);
 }
--- src/displays/library/library.js
+++ src/displays/library/library.js
@@ -1,7 +1,5 @@
-import React from 'react'
+import React from "react";
 
 export default function Library() {
-  return (
-    <div>Library</div>
-  )
+	return <div className="screen-container">Library</div>;
 }
