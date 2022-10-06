c8402637a68047b071a082d0d4d4487849fed239
Daniel Macharia <danielhuios@gmail.com>
2022-10-06 10:10:42 +0300

    fetch playlist data from spotify

--- src/displays/player/player.js
+++ src/displays/player/player.js
@@ -1,5 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import apiClient from "../../spotifyApi";
 
 export default function Player() {
-	return <div className="screen-container">Player</div>;
+	const location = useLocation();
+	const [tracks, setTracks] = useState([]);
+
+	useEffect(() => {
+		if (location.state) {
+			apiClient
+				.get("playlists/" + location.state?.id + "/tracks")
+				.then((res) => {
+					console.log(res.data);
+				});
+		}
+	}, []);
+
+	return (
+		<div className="screen-container">
+			<div className="left-player-body"></div>
+			<div className="left-player-body"></div>
+		</div>
+	);
 }
